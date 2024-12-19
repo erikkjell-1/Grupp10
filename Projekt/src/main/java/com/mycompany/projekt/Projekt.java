f3ccd40f15677bf4120e387b89f6121e88778097
erikkjell-1 <erkan26@live.se>
2024-12-19 10:43:01 +0100

    Skapade ett projekt

---
+++ Projekt/src/main/java/com/mycompany/projekt/Projekt.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projekt;
+
+/**
+ *
+ * @author Admin
+ */
+public class Projekt {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
